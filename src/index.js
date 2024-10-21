e215aa4f881b003738645da37f74ac9ffdd888a0
mohamad <mh711748@gmail.com>
2024-10-21 15:24:32 +0330

    time-style

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './style.css'
 
 
 class Timer extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      time : new Date().toLocaleTimeString()
+    }
+  }
   render(){
+    setInterval(()=>{
+      this.setState({
+        time : new Date().toLocaleTimeString()
+      })
+    } , 1000)
     return(
-      <h2>
-      it is {new Date().toLocaleTimeString()}
+      <h2 className="timer">
+      it is {this.state.time}
     </h2>
     )
   }
@@ -25,7 +36,7 @@ class Hello extends React.Component{
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="main">
        <Hello/>
        <Timer/>
       </div>
@@ -35,13 +46,13 @@ class App extends React.Component {
 
 
 
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const tick = () => {
+/*const tick = () => {
 
-  ReactDOM.render(<App />, document.getElementById('root'));
 }
 setInterval(() => {
   tick();
-}, 1000)
+}, 1000)*/
 
 
