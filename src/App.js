7219259af6d03a03719a7538d96b6efc04a096d0
mohamad <mh711748@gmail.com>
2024-10-24 17:28:21 +0330

    savetime

--- src/App.js
+++ src/App.js
@@ -3,24 +3,29 @@ import ReactDOM from 'react-dom';
 import './style.css'
 import Hello from './Hello';
 import Timer from './Timer';
+import TimeList from './TimeList';
 
 const App = ()=>{
   const [title , setTitle] = useState("سلام دوستان عزیزم");
   const [isLight , setisLight] = useState(false);
+  const [timeArr , setTimeArr] = useState(["00 : 05 : 12 " , "00 : 06 : 32"])
 
 
-useEffect(()=>{
+/*useEffect(()=>{
 console.log("useEffect");
+return ()=>{
+  
+}
 
 },[isLight])
-
+*/
   const handleSetIsLight = ()=>{
     setisLight(!isLight)
   }
   return(
     <div className="main" style={{background:isLight ? "white" : "black"}}>
     <Hello title={title} />
-    <Timer isLight={isLight} x={handleSetIsLight} />
+    <Timer timeArr={timeArr} setTimeArr={setTimeArr} isLight={isLight} x={handleSetIsLight} />
   </div>
   )
 }
--- src/Timer.js
+++ src/Timer.js
@@ -1,86 +1,100 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css'
+import TimeList from './TimeList';
 
 var interval;
 
-class Timer extends React.Component{
-  constructor(){
+class Timer extends React.Component {
+  constructor() {
     super()
     this.state = {
-      hour:0,
-      minute:0,
-      second:0,
-      isStart:false
+      hour: 0,
+      minute: 0,
+      second: 0,
+      isStart: false
     }
   }
 
 
-startInterval = ()=>{
-if (this.state.isStart== false){
-  this.setState({
-    isStart: true
-  })
-  interval = setInterval(()=>{
+  startInterval = () => {
+    if (this.state.isStart == false) {
+      this.setState({
+        isStart: true
+      })
+      interval = setInterval(() => {
+        this.setState({
+          second: this.state.second + 1
+        })
+        if (this.state.second == 60) {
+          this.setState({
+            second: 0,
+            minute: this.state.minute + 1
+          })
+        }
+        if (this.state.minute == 60) {
+          this.setState({
+            minute: 0,
+            hour: this.state.hour + 1
+          })
+        }
+      }, 1000)
+    }
+  }
+
+
+
+
+  stopInterval = () => {
     this.setState({
-      second : this.state.second + 1
+      isStart: false
     })
-    if (this.state.second == 60){
-      this.setState({
-        second : 0,
-        minute: this.state.minute + 1
-      })
-    }
-    if (this.state.minute == 60){
-      this.setState({
-        minute : 0,
-        hour: this.state.hour + 1
-      })
-    }
-  } , 1000)
-}
-}
+    clearInterval(interval);
+  }
+
+  resetInterval = () => {
+    this.stopInterval();
+    this.setState({
+      hour: 0,
+      minute: 0,
+      second: 0,
+    })
+  }
+
+  handleSaveTime = () => {
+    let h = this.state.hour
+    let m = this.state.minute
+    let s = this.state.second
+    let newTime = `${h > 9 ? h : "0" + h} : ${m > 9 ? m : "0" + m} : ${s > 9 ? s : "0" + s}`
+    this.props.setTimeArr([... this.props.timeArr , newTime])
+  }
 
 
 
 
-stopInterval = ()=>{
-  this.setState({
-    isStart: false})
-clearInterval(interval);
-}
-
-resetInterval = ()=>{
-this.stopInterval();
-this.setState({
-  hour:0,
-  minute:0,
-  second:0,
-})
-}
-
-
-
-
-  render(){
-let h = this.state.hour
-let m = this.state.minute
-let s = this.state.second
-    return(
+  render() {
+    let h = this.state.hour
+    let m = this.state.minute
+    let s = this.state.second
+    return (
       <>
-      <h2 className="timer">
-      {`${h > 9 ? h : "0"+h} : ${m > 9 ? m : "0"+m} : ${s > 9 ? s : "0"+s}`}
-    </h2>
-    <div className="button_box">
-      <span className="action_button start_burtton" onClick={this.startInterval}>start</span>
-      <span className="action_button stop_burtton" onClick={this.stopInterval}>stop</span>
-      <span className="action_button reset_burtton" onClick={this.resetInterval}>reset</span>
-      <span className="action_button reset_burtton" onClick={this.props.x}
-      style={{background:this.props.isLight ? "black" : "white" , 
-        color : this.props.isLight ? "white" : "black"
-      }}>{this.props.isLight ? "dark" : "light"}</span>
-    </div>
-    </>
+        <h2 className="timer" onClick={this.handleSaveTime}>
+          {`${h > 9 ? h : "0" + h} : ${m > 9 ? m : "0" + m} : ${s > 9 ? s : "0" + s}`}
+        </h2>
+        <div className="button_box">
+          <span className="action_button start_burtton" onClick={this.startInterval}>start</span>
+          <span className="action_button stop_burtton" onClick={this.stopInterval}>stop</span>
+          <span className="action_button reset_burtton" onClick={this.resetInterval}>reset</span>
+          <span className="action_button reset_burtton" onClick={this.props.x}
+            style={{
+              background: this.props.isLight ? "black" : "white",
+              color: this.props.isLight ? "white" : "black"
+            }}>{this.props.isLight ? "dark" : "light"}</span>
+        </div>
+        <TimeList >
+          {this.props.timeArr}
+        </TimeList>
+      </>
     )
   }
 }
